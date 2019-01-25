f4f9fa4ae7ee178f208b60cf52472b4dfb942f35
Anika Abreu <anika.abreu9@gmail.com>
2019-01-25 16:28:14 -0500

    deploy

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -44,7 +44,7 @@ export default {
 let token1 = window.PDK.getSession().accessToken
   function test(){
     axios.post('https://api.pinterest.com/v1/oauth/token?grant_type=authorization_codeclient_id='+'5012974284748908469' +
-    '&client_secret=' + process.env.VUE_SECRET + '&code=' + token1)
+    '&client_secret=' + VUE_SECRET + '&code=' + token1)
       .then(response => {
         console.log(response.data)
         // this.accessToken = response.data
