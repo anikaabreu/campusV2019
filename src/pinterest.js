cbf4b78dacd9a5f8134f1a8c9f8c86fe0cbbe362
Anika Abreu <anika.abreu9@gmail.com>
2019-01-26 19:59:51 -0500

    catch up deploy

--- src/pinterest.js
+++ src/pinterest.js
@@ -1,39 +1,39 @@
-import Vue from 'vue'
-import jquery from 'jquery';
+// import Vue from 'vue'
+// import jquery from 'jquery';
 
-var request = require("request");
-export default {
+// var request = require("request");
+// export default {
 
-    async execute() {
+//     async execute() {
 
-        let options = {
-            method: 'GET',
-            url: 'https://api.pinterest.com/v1/me/pins/',
-            qs: {
-                access_token: [
-                    window.PDK.getSession().accessToken
-                ],
-                fields: 'link%2Curl%2Cboard%2Ccounts%2Cmedia%2Coriginal_link%2Ccreator'
-            },
-            headers: {
-                'Postman-Token': '9b5f4e04-4a1a-44a5-bb3f-382ee4fe9cdc',
-                'cache-control': 'no-cache'
-            }
-        };
+//         let options = {
+//             method: 'GET',
+//             url: 'https://api.pinterest.com/v1/me/pins/',
+//             qs: {
+//                 access_token: [
+//                     window.PDK.getSession().accessToken
+//                 ],
+//                 fields: 'link%2Curl%2Cboard%2Ccounts%2Cmedia%2Coriginal_link%2Ccreator'
+//             },
+//             headers: {
+//                 'Postman-Token': '9b5f4e04-4a1a-44a5-bb3f-382ee4fe9cdc',
+//                 'cache-control': 'no-cache'
+//             }
+//         };
 
-        let arr = []
-        request(options, function(error, response, body) {
-            if (error) throw new Error(error);
+//         let arr = []
+//         request(options, function(error, response, body) {
+//             if (error) throw new Error(error);
 
-            console.log(body);
-            jquery(arr).push(body)
+//             console.log(body);
+//             jquery(arr).push(body)
 
 
-        })
+//         })
 
-    },
-    test() {
-        return arr
-    }
+//     },
+//     test() {
+//         return arr
+//     }
 
-}
+// }
