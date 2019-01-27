58a8ed4e02a1b0a63d3eccdbbc11c54d00abe796
Anika Abreu <anika.abreu9@gmail.com>
2019-01-27 00:39:07 -0500

    configured server firebase

--- src/components/PinterestBoards.vue
+++ src/components/PinterestBoards.vue
@@ -19,6 +19,7 @@ export default {
       model: {}
     }
   },
+
   // async created () {
   //   this.refreshPosts()
   // },
--- src/pinterest.js
+++ src/pinterest.js
@@ -1,39 +1,25 @@
-// import Vue from 'vue'
-// import jquery from 'jquery';
-
-// var request = require("request");
-// export default {
-
-//     async execute() {
-
-//         let options = {
-//             method: 'GET',
-//             url: 'https://api.pinterest.com/v1/me/pins/',
-//             qs: {
-//                 access_token: [
-//                     window.PDK.getSession().accessToken
-//                 ],
-//                 fields: 'link%2Curl%2Cboard%2Ccounts%2Cmedia%2Coriginal_link%2Ccreator'
-//             },
-//             headers: {
-//                 'Postman-Token': '9b5f4e04-4a1a-44a5-bb3f-382ee4fe9cdc',
-//                 'cache-control': 'no-cache'
-//             }
-//         };
-
-//         let arr = []
-//         request(options, function(error, response, body) {
-//             if (error) throw new Error(error);
-
-//             console.log(body);
-//             jquery(arr).push(body)
+var request = require("request");
 
 
-//         })
+let options = {
+    method: 'GET',
+    url: 'https://api.pinterest.com/v1/me/pins/',
+    qs: {
+        access_token: [
+            window.PDK.getSession().accessToken
+        ],
+        fields: 'link%2Curl%2Cboard%2Ccounts%2Cmedia%2Coriginal_link%2Ccreator'
+    },
+    headers: {
+        'Postman-Token': '9b5f4e04-4a1a-44a5-bb3f-382ee4fe9cdc',
+        'cache-control': 'no-cache'
+    }
+};
 
-//     },
-//     test() {
-//         return arr
-//     }
+let arr = []
+request(options, function(error, response, body) {
+    if (error) throw new Error(error);
 
-// }
+    console.log(body);
+
+})
