e4b56f5139ee43a927a688391d0ecfb1b8bd8b6a
Anika Abreu <anika.abreu9@gmail.com>
2019-01-27 19:50:22 -0500

    server config

--- src/components/PinterestBoards.vue
+++ src/components/PinterestBoards.vue
@@ -38,8 +38,7 @@ export default {
     })
 this.$firebaseRefs.cat.on("child_added", function(snapshot) {
       //change for production so no console.log
-    console.log(snapshot.val());
-    console.log('old', "AobgfDBynql0vF61hZn4NhVtNjsJFX3WXXIDbCBFk0CuTMBy-QakwDAAAHpHRZO-CxpgXoUAAAAA")
+    // console.log(snapshot.val());
 });
 
 }
