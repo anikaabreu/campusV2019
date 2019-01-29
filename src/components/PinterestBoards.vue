a62f883d2d9bef85d88ca60dd8ad83d8e8b1d79b
Anika Abreu <anika.abreu9@gmail.com>
2019-01-29 00:58:38 -0500

    removed icons

--- src/components/PinterestBoards.vue
+++ src/components/PinterestBoards.vue
@@ -14,24 +14,6 @@
        Choose Users or Pins to get started and look at your data unsorted. Tables will provide a sorted view.
       </v-card-text>
 
-      <v-card-actions>
-        <v-list-tile class="grow">
-          <v-list-tile-avatar color="grey darken-3">
-
-          </v-list-tile-avatar>
-
-          <v-layout
-            align-center
-            justify-end
-          >
-            <v-icon class="mr-1">mdi-heart</v-icon>
-            <span class="subheading mr-2">256</span>
-            <span class="mr-1">·</span>
-            <v-icon class="mr-1">mdi-share-variant</v-icon>
-            <span class="subheading">45</span>
-          </v-layout>
-        </v-list-tile>
-      </v-card-actions>
     </v-card>
     <router-view></router-view>
 
