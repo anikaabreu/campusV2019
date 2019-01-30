76752d389737d9eee99ad950bc05aa184ea2280a
simpleshow <simpleshow@Anikas-iMac.local>
2019-01-30 11:53:26 -0500

    deploy errors

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-import PostsManager from '@/components/PostsManager'
 import PinterestBoards from '@/components/PinterestBoards'
 import Pins from '@/components/Pins'
 import Users from '@/components/Users'
@@ -19,27 +18,16 @@ export default new Router({
             component: Hello
         },
         {
-            path: '/posts-manager',
-            name: 'PostsManager',
-            component: PostsManager,
-            meta: {
-
-            }
-        },
-        {
             path: '/pinterest-boards',
             name: 'PinterestBoards',
             component: PinterestBoards,
             children: [{
-                    // UserProfile will be rendered inside User's <router-view>
-                    // when /pinrerest-boards/pins is matched
+                   
                     path: 'pins',
                     component: Pins,
 
                 },
-                {
-                    // UserPosts will be rendered inside User's <router-view>
-                    // when /pinrerest-boards/users is matched
+                { 
                     path: 'users',
                     component: Users
                 }, {
